089beccb2493dea52305a83571a54809f412ee52
Alberto Galue <albertogalue@gmail.com>
2026-04-24 12:44:02 -0400

    Update SCHEMA.sql

--- sql/SCHEMA.sql
+++ sql/SCHEMA.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS transactions (
 transaction_id SERIAL PRIMARY KEY,
 account_id INT NOT NULL,
 transaction_date DATE NOT NULL DEFAULT CURRENT_DATE,
-transaction_type TEXT NOT NULL,
-amount DECIMAL (15,2),  CHECK (amount >= 0),
-currency VARCHAR (5) DEFAULT 'usd',
+transaction_type TEXT NOT NULL CHECK (transaction_type IN ('Deposit','Withdrawal')),
+amount DECIMAL (15,2) NOT NULL CHECK (amount >= 0),
+currency VARCHAR (5) NOT NULL DEFAULT 'USD',
 category TEXT
 );
 
